b6273415c8dbcc791b8e0a5ea04d7d0a7aa0f95f
ecrou-exact <geffetheo@gmail.com>
2025-07-23 08:21:07 +0200

    fix:

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
+sudo apt install python3.12-venv
 # install python requirements
 python3 -m venv env
 . env/bin/activate
 pip install -r requirements.txt
-# init submodules
-# git submodule init && git submodule update
+
 
 ./install_postgresql.sh
 
