857379dd2b16ead95cd73a1cf41d35b484082bc4
ecrou-exact <geffetheo@gmail.com>
2025-10-20 08:42:14 +0200

    fix: [install script] english only

--- install.sh
+++ install.sh
@@ -5,25 +5,21 @@ GREEN="\033[0;32m"
 CYAN="\033[0;36m"
 RESET="\033[0m"
 
-echo -e "${CYAN}🔧 Installation de python3.12-venv...${RESET}"
+echo -e "${CYAN}🚀 Starting the installation process...${RESET}"
+
 sudo apt install -y python3.12-venv
-
-echo -e "${CYAN}🐍 Création de l'environnement virtuel...${RESET}"
 python3 -m venv env
-
-echo -e "${CYAN}✅ Activation de l'environnement virtuel...${RESET}"
 . env/bin/activate
 
-echo -e "${CYAN}📦 Installation des dépendances Python...${RESET}"
+echo -e "${CYAN}📦 Install the Python dependencies...${RESET}"
 pip install -r requirements.txt
 
-echo -e "${CYAN}🐘 Installation de PostgreSQL via le script dédié...${RESET}"
+echo -e "${CYAN}🐘 Install PostgreSQL ...${RESET}"
 ./install_postgresql.sh
 
-echo -e "${CYAN}🚀 Rend le script de lancement exécutable...${RESET}"
 chmod +x ./launch.sh
 
 . env/bin/activate
 
-echo -e "${GREEN}🎮 Lancement de l'application...${RESET}"
+echo -e "${GREEN}🎮 Launch the application...${RESET}"
 ./launch.sh -i
