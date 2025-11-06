76796834cd49c17523f1693b048ace63e52ee7e2
ecrou-exact <geffetheo@gmail.com>
2025-11-06 14:25:11 +0100

    fix: [bundle public private]

--- app/bundle/bundle_core.py
+++ app/bundle/bundle_core.py
@@ -279,16 +279,21 @@ def get_rules_from_bundle(bundle_id: int) -> List[Rule]:
 
 def get_bundles_by_rule(rule_id: int) -> List[Bundle]:
     """
-    Retrieve all bundles that contain a specific rule.
+    Retrieve all bundles that contain a specific rule and are publicly accessible.
+    
     :param rule_id: ID of the rule to search for.
-    :return: List of Bundle instances containing the specified rule.
+    :return: List of Bundle instances containing the specified rule and with access=True.
     """
     return (
         db.session.query(Bundle)
         .join(BundleRuleAssociation, BundleRuleAssociation.bundle_id == Bundle.id)
-        .filter(BundleRuleAssociation.rule_id == rule_id)
+        .filter(
+            BundleRuleAssociation.rule_id == rule_id,
+            Bundle.access.is_(True)  # Only include bundles where access == True
+        )
         .all()
     )
+
 
 def toggle_bundle_accessibility(bundle_id: int) -> bool:
     """
